28b81e2ce55c37a474d6c1e19d699da24595e253
Anhy <anahi.z.a@gmail.com>
2017-01-04 18:36:27 -0600

    Terminar rutina y test para guardar usuario en la base

--- test/db-test.js
+++ test/db-test.js
@@ -4,6 +4,7 @@ const Db = require('../')
 const uuid = require('uuid-base62')
 const r = require('rethinkdb')
 const fixtures = require('./fixtures')
+const utils = require('../lib/utils')
 
 test.beforeEach('setup database', async t => {
   const dbName = `platzigram_${uuid.v4()}`
@@ -65,4 +66,18 @@ test('list all images', async t => {
   let created = await Promise.all(saveImages)
   let result = await db.getImages()
   t.is(created.length, result.length)
+})
+
+test('save user', async t => {
+  let db = t.context.db
+  t.is(typeof db.saveUser, 'function', 'saveUser is a function')
+  let user = fixtures.getUser()
+  let plainPassword = user.password
+  let created = await db.saveUser(user)
+  t.is(user.username, created.username)
+  t.is(user.email, created.email)
+  t.is(user.name, created.name)
+  t.is(utils.encrypt(plainPassword), created.password)
+  t.is(typeof created.id, 'string')
+  t.truthy(created.createdAt)
 })
